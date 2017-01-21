900a155255f3f74b8bd78fa6450fc88e14952ca7
clfinch <clfinch@mtu.edu>
2017-01-20 19:52:55 -0500

    Init Project

---
+++ app/src/main/java/com/cody/renttracker/RentTrackerActivity.java
@@ -0,0 +1,13 @@
+package com.cody.renttracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RentTrackerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rent_tracker);
+    }
+}
